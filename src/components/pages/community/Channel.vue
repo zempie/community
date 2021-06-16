1fd0a7506c936e357967be59d5096e80dda3f085
Hyeonjeong Lee <hjeong>
2021-06-17 00:05:35 +0900

    커뮤니티 채널 탭 수정

--- src/components/pages/community/Channel.vue
+++ src/components/pages/community/Channel.vue
@@ -1,72 +1,66 @@
 <template>
-    <div
-        class="user-status-list"
-        @click="moveChannel"
-        :class="active ? 'active' : ''"
-        :id="channel.id"
-    >
-        <span class="channel-title">{{ channel.name }}</span>
-        <img class="channel-img" :src="`${channel.profile_img}`" />
-    </div>
+  <div
+    class="user-status-list"
+    @click="moveChannel(channel.id)"
+    :id="channel.id"
+  >
+    <span class="channel-title">{{ channel.name }}</span>
+    <img class="channel-img" :src="`${channel.profile_img}`" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
-
 @Component({
-    components: {},
+  components: {},
 })
 export default class Channel extends Vue {
-    @Prop() channel!: any;
-    @Prop() communityId!: number;
-    private active: boolean = false;
-    private timeline: any;
-    // private isClickedChannel: boolean = false;
+  @Prop() channel!: any;
+  @Prop() communityId!: number;
 
-    mounted() {
-        // console.log("mounted", this.clicked);
-    }
+  private timeline: any;
+  private channelId: any = -1;
 
-    moveChannel(id?: number) {
-        console.log("channel", this.channel.id, this.$el.id);
-        this.timeline = this.$api.getTimeline(this.communityId, this.channel.id);
-        this.$emit('channelTimeline', this.timeline)
-    }
+  moveChannel(id?: number) {
+    this.timeline = this.$api.getTimeline(this.communityId, this.channel.id);
+    this.$emit("channelId", id);
+    this.$emit("channelTimeline", this.timeline);
+  }
 }
 </script>
 
 <style scoped>
 .channel-img {
-    width: 100%;
-    height: 65px;
-    border-radius: 7px;
-    margin-bottom: 10px;
-    opacity: 50%;
+  width: 100%;
+  height: 65px;
+  border-radius: 7px;
+  margin-bottom: 10px;
+  opacity: 50%;
 }
 .channel-img:hover,
 .user-status-list.active .channel-img {
-    opacity: 100%;
-    cursor: pointer;
+  opacity: 100%;
+  cursor: pointer;
 }
 .user-status-list.active .channel-title {
-    background-color: #fff;
-    color: #1d2333;
+  background-color: #fff;
+  color: #1d2333;
 }
 .channel-title {
-    border-radius: 6px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    font-weight: 600;
-    padding: 2px 6px;
-    background-color: #1d2333;
-    color: #fff;
-    position: absolute;
-    z-index: 3;
-    margin: 8px;
-    display: flex;
-    justify-content: space-between;
-    font-family: "Rajdhani", sans-serif;
+  border-radius: 6px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-weight: 600;
+  padding: 2px 6px;
+  background-color: #1d2333;
+  color: #fff;
+  position: absolute;
+  z-index: 3;
+  margin: 8px;
+  display: flex;
+  justify-content: space-between;
+  font-family: "Rajdhani", sans-serif;
 }
 </style>
--- src/components/pages/community/CommunityTimeline.vue
+++ src/components/pages/community/CommunityTimeline.vue
@@ -1,46 +1,46 @@
 <template>
-    <div class="grid grid-3-6-3 mobile-prefer-content">
-        <div class="grid-column">
-            <div class="widget-box">
-                <p class="widget-box-title">Channels</p>
-                <div class="widget-box-content">
-                    <div
-                        class="user-status-list"
-                        :class="isAllPosts ? 'active' : ''"
-                        @click="allPost"
-                    >
-                        <span class="channel-title">All Posts</span>
-                        <img class="channel-img" />
-                    </div>
-                    <div @click="isAllPosts = false">
-                        <channel
-                            :channel="channel"
-                            v-for="channel in community.channels"
-                            :key="channel.id"
-                            :communityId="communityId"
-                            @channelTimeline="channelTimeline"
-                        ></channel>
-                    </div>
-                </div>
-            </div>
+  <div class="grid grid-3-6-3 mobile-prefer-content">
+    <div class="grid-column">
+      <div class="widget-box">
+        <p class="widget-box-title">Channels</p>
+        <div class="widget-box-content">
+          <div
+            class="user-status-list"
+            :class="{ active: isActive(-1) }"
+            @click="allPost"
+          >
+            <!-- todo: 이미지 있없 -->
+            <span class="channel-title">All Posts</span>
+            <div class="channel-img"></div>
+            <!-- <img  /> -->
+          </div>
+          <div @click="isActive()">
+            <channel
+              :channel="channel"
+              v-for="channel in community.channels"
+              :key="channel.id"
+              :communityId="communityId"
+              :class="{ active: isActive(channel.id) }"
+              @channelId="getChannelId"
+              @channelTimeline="getChanneTL"
+            ></channel>
+          </div>
         </div>
-        <div class="grid-column">
-            <post></post>
-            <!-- 타임라인 -->
-            <template v-if="timeline">
-                <feed
-                    v-for="feed in timeline"
-                    :key="feed.id"
-                    :feed="feed"
-                ></feed>
-            </template>
-            <template v-if="timeline.length === 0">
-                <!-- todo: design -->
-                <h2>No Result</h2>
-            </template>
-        </div>
-        <div class="grid-column"></div>
+      </div>
     </div>
+    <div class="grid-column">
+      <post></post>
+      <!-- 타임라인 -->
+      <template v-if="timeline">
+        <feed v-for="feed in timeline" :key="feed.id" :feed="feed"></feed>
+      </template>
+      <template v-if="timeline.length === 0">
+        <!-- todo: design -->
+        <h2>No Result</h2>
+      </template>
+    </div>
+    <div class="grid-column"></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -52,69 +52,80 @@ import Channel from "@/components/pages/community/Channel.vue";
 import Feed from "@/components/timeline/Feed.vue";
 
 @Component({
-    components: { Post, Channel, Feed },
+  components: { Post, Channel, Feed },
 })
 export default class CommunityTimeline extends Vue {
-    private dropdown: Dropdown = new Dropdown();
-    private communityId = parseInt(this.$route.params.community_id);
-    private community: any;
-    private timeline: any;
-    private isAllPosts: boolean = false;
+  private dropdown: Dropdown = new Dropdown();
+  private communityId = parseInt(this.$route.params.community_id);
+  private community: any;
+  private timeline: any;
+  private isAllPosts: boolean = false;
 
-    created() {
-        this.isAllPosts = true;
-        this.community = this.$api.getCommunityInfo(this.communityId);
-        //all posts
-        this.timeline = this.$api.getTimeline(this.communityId);
-    }
-    mounted() {
-        this.dropdown.init();
-    }
+  private channelId: number = -1;
 
-    allPost() {
-        this.isAllPosts = true;
-        this.timeline = this.$api.getTimeline(this.communityId);
-    }
+  created() {
+    this.isAllPosts = true;
+    this.community = this.$api.getCommunityInfo(this.communityId);
+    //all posts
+    this.timeline = this.$api.getTimeline(this.communityId);
+  }
+  mounted() {
+    this.dropdown.init();
+  }
 
-    channelTimeline(value: any) {
-        this.timeline = value;
-        console.log("emit", value);
-    }
+  allPost() {
+    this.channelId = -1;
+    this.isActive(-1);
+    this.timeline = this.$api.getTimeline(this.communityId);
+  }
+  getChannelId(id?: any) {
+    this.channelId = id;
+  }
+  getChanneTL(timeline: any) {
+    this.timeline = timeline;
+  }
+
+  isActive(id?: any) {
+    return id == this.channelId;
+  }
 }
 </script>
 
 <style scoped>
 .channel-img {
-    width: 100%;
-    height: 65px;
-    margin-bottom: 10px;
-    background-color: #343a40;
-    opacity: 50%;
+  width: 100%;
+  height: 65px;
+  margin-bottom: 10px;
+  background-color: #343a40;
+  opacity: 50%;
 }
 .channel-img:hover,
 .user-status-list.active .channel-img {
-    opacity: 100%;
-    cursor: pointer;
+  opacity: 100%;
+  cursor: pointer;
 }
 .user-status-list.active .channel-title {
-    background-color: #fff;
-    color: #1d2333;
+  background-color: #fff;
+  color: #1d2333;
 }
 
 .channel-title {
-    border-radius: 6px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    font-weight: 600;
-    padding: 2px 6px;
-    background-color: #1d2333;
-    color: #fff;
-    position: absolute;
-    z-index: 3;
-    margin: 8px;
-    display: flex;
-    justify-content: space-between;
-    font-family: "Rajdhani", sans-serif;
+  border-radius: 6px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-weight: 600;
+  padding: 2px 6px;
+  background-color: #1d2333;
+  color: #fff;
+  position: absolute;
+  z-index: 3;
+  margin: 8px;
+  display: flex;
+  justify-content: space-between;
+  font-family: "Rajdhani", sans-serif;
+}
+.channel-img {
+  border-radius: 7px;
 }
 </style>
