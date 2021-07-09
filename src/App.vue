f33e78db5e968aea6d394fbd6a53e3dc86744a71
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2021-07-09 12:54:47 +0900

    font noto sans

--- src/App.vue
+++ src/App.vue
@@ -45,12 +45,12 @@ export default class App extends Vue {
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap");
 
 #app {
-    font-family: Rajdhani, "Noto Sans KR", "sans-serif";
+    font-family: "Noto Sans KR", "sans-serif";
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    
+
     // margin-top: 60px;
 }
 
